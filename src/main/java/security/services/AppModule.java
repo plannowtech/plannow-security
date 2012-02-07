8a12adb3332756cd23533208bd34bcb26a791575
Dusko Jovanovski <duskote@gmail.com>
2012-02-07 16:23:11 +0100

    Introducing mail server symbols
    Signed-off-by: Dusko Jovanovski <duskote@gmail.com>

--- src/main/java/security/services/AppModule.java
+++ src/main/java/security/services/AppModule.java
@@ -2,16 +2,20 @@ package security.services;
 
 import java.io.IOException;
 
-import org.apache.tapestry5.*;
+import org.apache.tapestry5.SymbolConstants;
 import org.apache.tapestry5.ioc.MappedConfiguration;
 import org.apache.tapestry5.ioc.OrderedConfiguration;
 import org.apache.tapestry5.ioc.ServiceBinder;
 import org.apache.tapestry5.ioc.annotations.Local;
+import org.apache.tapestry5.ioc.annotations.Startup;
+import org.apache.tapestry5.ioc.annotations.Symbol;
 import org.apache.tapestry5.services.Request;
 import org.apache.tapestry5.services.RequestFilter;
 import org.apache.tapestry5.services.RequestHandler;
 import org.apache.tapestry5.services.Response;
 import org.slf4j.Logger;
+
+import security.model.PlannowSecuritySymbols;
 
 /**
  * This module is automatically included as part of the Tapestry IoC Registry, it's a good place to
@@ -19,99 +23,111 @@ import org.slf4j.Logger;
  */
 public class AppModule
 {
-    public static void bind(ServiceBinder binder)
-    {
-        // binder.bind(MyServiceInterface.class, MyServiceImpl.class);
+	public static void bind(ServiceBinder binder)
+	{
+		// binder.bind(MyServiceInterface.class, MyServiceImpl.class);
 
-        // Make bind() calls on the binder object to define most IoC services.
-        // Use service builder methods (example below) when the implementation
-        // is provided inline, or requires more initialization than simply
-        // invoking the constructor.
-    }
+		// Make bind() calls on the binder object to define most IoC services.
+		// Use service builder methods (example below) when the implementation
+		// is provided inline, or requires more initialization than simply
+		// invoking the constructor.
+	}
 
-    public static void contributeFactoryDefaults(
-            MappedConfiguration<String, Object> configuration)
-    {
-        // The application version number is incorprated into URLs for some
-        // assets. Web browsers will cache assets because of the far future expires
-        // header. If existing assets are changed, the version number should also
-        // change, to force the browser to download new versions. This overrides Tapesty's default
-        // (a random hexadecimal number), but may be further overriden by DevelopmentModule or
-        // QaModule.
-        configuration.override(SymbolConstants.APPLICATION_VERSION, "1.0-SNAPSHOT");
-    }
+	public static void contributeFactoryDefaults(MappedConfiguration<String, Object> configuration)
+	{
+		// The application version number is incorporated into URLs for some
+		// assets. Web browsers will cache assets because of the far future expires
+		// header. If existing assets are changed, the version number should also
+		// change, to force the browser to download new versions. This overrides Tapesty's default
+		// (a random hexadecimal number), but may be further overridden by DevelopmentModule or
+		// QaModule.
+		configuration.override(SymbolConstants.APPLICATION_VERSION, "1.0-SNAPSHOT");
 
-    public static void contributeApplicationDefaults(
-            MappedConfiguration<String, Object> configuration)
-    {
-        // Contributions to ApplicationDefaults will override any contributions to
-        // FactoryDefaults (with the same key). Here we're restricting the supported
-        // locales to just "en" (English). As you add localised message catalogs and other assets,
-        // you can extend this list of locales (it's a comma separated series of locale names;
-        // the first locale name is the default when there's no reasonable match).
-        configuration.add(SymbolConstants.SUPPORTED_LOCALES, "en");
-    }
+		// default values for gmail smtp server
+		// these can be overridden in the application module
+		// additionally add configuration entries for PlannowSecuritySymbols.MAIL_ADDRESS,
+		// PlannowSecuritySymbols.MAIL_USERNAME and PlannowSecuritySymbols.MAIL_PASSWORD
+		configuration.add(PlannowSecuritySymbols.MAIL_HOST, "smtp.gmail.com");
+		configuration.add(PlannowSecuritySymbols.MAIL_PORT, "587");
 
+	}
 
-    /**
-     * This is a service definition, the service will be named "TimingFilter". The interface,
-     * RequestFilter, is used within the RequestHandler service pipeline, which is built from the
-     * RequestHandler service configuration. Tapestry IoC is responsible for passing in an
-     * appropriate Logger instance. Requests for static resources are handled at a higher level, so
-     * this filter will only be invoked for Tapestry related requests.
-     * <p/>
-     * <p/>
-     * Service builder methods are useful when the implementation is inline as an inner class
-     * (as here) or require some other kind of special initialization. In most cases,
-     * use the static bind() method instead.
-     * <p/>
-     * <p/>
-     * If this method was named "build", then the service id would be taken from the
-     * service interface and would be "RequestFilter".  Since Tapestry already defines
-     * a service named "RequestFilter" we use an explicit service id that we can reference
-     * inside the contribution method.
-     */
-    public RequestFilter buildTimingFilter(final Logger log)
-    {
-        return new RequestFilter()
-        {
-            public boolean service(Request request, Response response, RequestHandler handler)
-                    throws IOException
-            {
-                long startTime = System.currentTimeMillis();
+	public static void contributeApplicationDefaults(
+			MappedConfiguration<String, Object> configuration)
+	{
+		// Contributions to ApplicationDefaults will override any contributions to
+		// FactoryDefaults (with the same key). Here we're restricting the supported
+		// locales to just "en" (English). As you add localized message catalogs and other assets,
+		// you can extend this list of locales (it's a comma separated series of locale names;
+		// the first locale name is the default when there's no reasonable match).
+		configuration.add(SymbolConstants.SUPPORTED_LOCALES, "en");
+	}
 
-                try
-                {
-                    // The responsibility of a filter is to invoke the corresponding method
-                    // in the handler. When you chain multiple filters together, each filter
-                    // received a handler that is a bridge to the next filter.
+	/**
+	 * This is a service definition, the service will be named "TimingFilter". The interface,
+	 * RequestFilter, is used within the RequestHandler service pipeline, which is built from the
+	 * RequestHandler service configuration. Tapestry IoC is responsible for passing in an
+	 * appropriate Logger instance. Requests for static resources are handled at a higher level, so
+	 * this filter will only be invoked for Tapestry related requests.
+	 * <p/>
+	 * <p/>
+	 * Service builder methods are useful when the implementation is inline as an inner class (as
+	 * here) or require some other kind of special initialization. In most cases, use the static
+	 * bind() method instead.
+	 * <p/>
+	 * <p/>
+	 * If this method was named "build", then the service id would be taken from the service
+	 * interface and would be "RequestFilter". Since Tapestry already defines a service named
+	 * "RequestFilter" we use an explicit service id that we can reference inside the contribution
+	 * method.
+	 */
+	public RequestFilter buildTimingFilter(final Logger log)
+	{
+		return new RequestFilter()
+		{
+			public boolean service(Request request, Response response, RequestHandler handler)
+					throws IOException
+			{
+				long startTime = System.currentTimeMillis();
 
-                    return handler.service(request, response);
-                } finally
-                {
-                    long elapsed = System.currentTimeMillis() - startTime;
+				try
+				{
+					// The responsibility of a filter is to invoke the corresponding method
+					// in the handler. When you chain multiple filters together, each filter
+					// received a handler that is a bridge to the next filter.
 
-                    log.info(String.format("Request time: %d ms", elapsed));
-                }
-            }
-        };
-    }
+					return handler.service(request, response);
+				}
+				finally
+				{
+					long elapsed = System.currentTimeMillis() - startTime;
 
-    /**
-     * This is a contribution to the RequestHandler service configuration. This is how we extend
-     * Tapestry using the timing filter. A common use for this kind of filter is transaction
-     * management or security. The @Local annotation selects the desired service by type, but only
-     * from the same module.  Without @Local, there would be an error due to the other service(s)
-     * that implement RequestFilter (defined in other modules).
-     */
-    public void contributeRequestHandler(OrderedConfiguration<RequestFilter> configuration,
-                                         @Local
-                                         RequestFilter filter)
-    {
-        // Each contribution to an ordered configuration has a name, When necessary, you may
-        // set constraints to precisely control the invocation order of the contributed filter
-        // within the pipeline.
+					log.info(String.format("Request time: %d ms", elapsed));
+				}
+			}
+		};
+	}
 
-        configuration.add("Timing", filter);
-    }
+	/**
+	 * This is a contribution to the RequestHandler service configuration. This is how we extend
+	 * Tapestry using the timing filter. A common use for this kind of filter is transaction
+	 * management or security. The @Local annotation selects the desired service by type, but only
+	 * from the same module. Without @Local, there would be an error due to the other service(s)
+	 * that implement RequestFilter (defined in other modules).
+	 */
+	public void contributeRequestHandler(OrderedConfiguration<RequestFilter> configuration,
+			@Local RequestFilter filter)
+	{
+		// Each contribution to an ordered configuration has a name, When necessary, you may
+		// set constraints to precisely control the invocation order of the contributed filter
+		// within the pipeline.
+
+		configuration.add("Timing", filter);
+	}
+
+	@Startup
+	public void startupMethod(@Symbol(PlannowSecuritySymbols.MAIL_ADDRESS) String mailAddress)
+	{
+		System.out.println(mailAddress);
+	}
 }
